877559418f193e528481124404e1f7d0db13210b
Milton <miltonacosta1101@gmail.com>
2024-11-01 19:56:53 -0500

    modificando el footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,33 +4,29 @@ import advertencia from "../../assets/perdidos.png";
 import lupa from "../../assets/encontrados.png";
 import informacion from "../../assets/info.png";
 import refugio from "../../assets/refugio.png";
-import logo from "../../assets/icono1.png";
+import logo from "../../assets/logoHAC7.png";
 
 export default function Footer() {
   return (
     <div>
       <div className={styles.containerFooter}>
-        <img src={logo} alt="logo de huellitas a casa" className={styles.logo}/>
         <ul>
           <li>
-            <img src={advertencia} alt="icono de seccion perdidos" />
             <a href="">Perdidos</a>
           </li>
           <li>
-            <img src={lupa} alt="icono de seccion encontrados" />
             <a href="">Encontrados</a>
           </li>
           <li>
-            <img src={refugio} alt="icono de seccion fundaciones" />
             <a href="">Fundaciones</a>
           </li>
           <li>
-            <img src={informacion} alt="icono de seccion informacion" />
             <a href="">Informacion</a>
           </li>
         </ul>
+        <img src={logo} alt="logo de huellitas a casa" className={styles.logo}/>
+        <p>Derechos de la pagina reservados</p>
       </div>
-      <p>Derechos de la pagina reservados</p>
     </div>
   )
 }
