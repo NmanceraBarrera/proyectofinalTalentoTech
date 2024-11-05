c58f5246abf2c3108c282c1613f7b80ea1380e2f
Alexander-Martinez-Developer <alexandermartinez.code@gmail.com>
2024-11-05 15:10:31 -0500

    cambios en pagina perdidos

--- src/components/perdidos/Perdidos.jsx
+++ src/components/perdidos/Perdidos.jsx
@@ -6,145 +6,93 @@ import ModalScreen from "../utils/Modal";
 export default function Perdidos() {
   return (
     <div>
-      <div className={styles.container_principal}></div>
       <Navbar />
       <ModalScreen />
+      <div className={styles.titulos_1}>
+        <h2>¿Encontraste un animal?</h2>
+        <h3>¿Que hacer?</h3>
+      </div>
+      {/* ---------------Seccion 1----------------- */}
       <section className={styles.section_1}>
-        <div className={styles.titulos_1}>
-          <h2>¿Encontraste un animal?</h2>
-          <h3>¿Qué hacer?</h3>
+        <div className={styles.informacion_1}>
+          <p>
+            Accede a la Seccion de "Mascotas Perdidas". Aqui podras ver la lista
+            de animales que han sido reportados como perdidos y asi buscar si
+            alguien lo esta buscando
+          </p>
+          <button>Perdidos</button>
         </div>
-        <div className={styles.container_info}>
-          <div className={styles.caja_info}>
-            <div className={styles.informacion}>
-              <div className={styles.contenido_informacion}>
-                <p>
-                  Accede a la Sección de "Mascotas Encontradas": Aquí puedes ver
-                  una lista de animales que han sido reportados por otros
-                  usuarios como encontrados en tu zona.
-                </p>
-                <div className={styles.contenedor_boton}>
-                  <button>Perdidos</button>
-                </div>
-              </div>
-            </div>
-            <div className={styles.imagen}>
-              <img
-                src="https://i.pinimg.com/236x/c3/b3/99/c3b399f88c45e47398ddea829b07e47c.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className={styles.caja_info_2}>
-            <div className={styles.informacion}>
-              <div className={styles.contenido_informacion}>
-                <p>
-                  Mantener la calma y tomar medidas inmediatas. Si deseas saber
-                  más sobre control de emociones en situaciones similares, te
-                  dejamos una guía rápida justo aquí abajo.
-                </p>
-                <div className={styles.contenedor_boton}>
-                  <button>¿Qué hacer?</button>
-                </div>
-              </div>
-            </div>
-            <div className={styles.imagen}>
-              <img
-                src="https://i.pinimg.com/236x/de/61/b0/de61b01eca182f94c92bce8b5cfef6d0.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className={styles.caja_info}>
-            <div className={styles.informacion}>
-              <div className={styles.contenido_informacion}>
-                <p>
-                  Si no encuentras a tu mascota en la sección de "Encontrados",
-                  es hora de registrar el caso.
-                </p>
-                <div className={styles.contenedor_boton}>
-                  <button>Encontrado</button>
-                </div>
-              </div>
-            </div>
-            <div className={styles.imagen}>
-              <img
-                src="https://i.pinimg.com/236x/47/50/c9/4750c969411d2b310d3631119057bc46.jpg"
-                alt=""
-              />
-            </div>
-          </div>
-          <div className={styles.caja_info_2}>
-            <div className={styles.informacion}>
-              <div className={styles.contenido_informacion}>
-                <p>
-                  {" "}
-                  Aquí podrás ver la lista de animales que han sido reportados
-                  como perdidos y así buscar si alguien lo está buscando.
-                </p>
-                <div className={styles.contenedor_boton}>
-                  <button>Fundaciones</button>
-                </div>
-              </div>
-            </div>
-            <div className={styles.imagen}>
-              <img
-                src="https://i.pinimg.com/236x/a1/f1/27/a1f127907b167e3046d17e0f1429bfc8.jpg"
-                alt=""
-              />
-            </div>
-          </div>
+        <div className={styles.imagen_1}>
+          <img
+            src="https://i.pinimg.com/564x/ef/97/90/ef9790a063fd9d4e72cb3b59aa0a3a6c.jpg"
+            alt="perros y gatos"
+          />
+        </div>
+      </section>
+      {/* -----------------------section_2------------ */}
+      <section className={styles.section_2}>
+        <div className={styles.informacion_2}>
+          <p>
+            Si nunca has cuidado una mascota antes o si sientes que necesitas un
+            poco de orientación, ¡no te preocupes!
+          </p>
+          <button>¿Qué hacer?</button>
+        </div>
+        <div className={styles.imagen_2}>
+          <img
+            src="https://i.pinimg.com/564x/47/ab/a2/47aba26ddeac4d80dcfa4b3d0cb12a27.jpg"
+            alt="perros y gatos"
+          />
         </div>
       </section>
 
+      {/* ---------------Seccion 3----------------- */}
+      <section className={styles.section_1}>
+        <div className={styles.informacion_1}>
+          <p>
+            Si no encuentras a la mascota en la sección de "Perdidos", es hora
+            de registrar el caso
+          </p>
+          <button>Registra</button>
+        </div>
+        <div className={styles.imagen_1}>
+          <img
+            src="https://i.pinimg.com/564x/2a/0a/b7/2a0ab78985cceb539ffb04a424c15c43.jpg"
+            alt="perros y gatos"
+          />
+        </div>
+      </section>
+      {/* -----------------------section_4------------ */}
       <section className={styles.section_2}>
-        <div className={styles.spinner}>
-          <div className={styles.titulo_2}>
-            <h2>
-              Aquí encontrarás todas las mascotas que se han agregado como
-              encontradas. Recuerda que puedes filtrar de acuerdo a ciertas
-              características.
-            </h2>
+        <div className={styles.informacion_2}>
+          <p>
+            Si no puedes cuidar de este(a), puedes comunicarte con alguna de las
+            fundaciones inscritas ¡a un solo click!
+          </p>
+          <button>Comunicate</button>
+        </div>
+        <div className={styles.imagen_2}>
+          <img
+            src="https://i.pinimg.com/236x/39/f7/3a/39f73a6c5ba8d45a209e8a94626eeaa5.jpg"
+            alt="perros y gatos"
+          />
+        </div>
+      </section>
+      {/* -----------------------section_5------------ */}
+      <section className={styles.section_5}>
+        <h3>
+          Aca encontraras todos las mascotas que se han agregado como
+          encontradas, recuerda que: puedes filtrar de acuerdo a ciertas
+          caracteristicas
+        </h3>
+        <div className={styles.barra_busqueda}>
+          <div className={styles.container_busqueda}>
+          <input type="text" name="nombre" id="nombre" placeholder="Buscar" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><defs><mask id="IconifyId192f9c235299a8f4b1"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M21 38c9.389 0 17-7.611 17-17S30.389 4 21 4S4 11.611 4 21s7.611 17 17 17Z"/><path stroke-linecap="round" d="M26.657 14.343A7.98 7.98 0 0 0 21 12a7.98 7.98 0 0 0-5.657 2.343m17.879 18.879l8.485 8.485"/></g></mask></defs><path fill="#eca883" d="M0 0h48v48H0z" mask="url(#IconifyId192f9c235299a8f4b1)"/></svg>
           </div>
-          <div className={styles.Container_spinner_section}>
-            <div className={styles.bar_busqueda}>
-              <div className={styles.busqueda}>
-                <input
-                  type="text"
-                  name="nombre"
-                  id="nombre"
-                  placeholder="Buscar"
-                />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
-                  <defs>
-                    <mask id="IconifyId192de9132498e907f4">
-                      <g
-                        fill="none"
-                        stroke="#fff"
-                        stroke-linejoin="round"
-                        strokeWidth="4"
-                      >
-                        <path
-                          fill="#555"
-                          d="M21 38c9.389 0 17-7.611 17-17S30.389 4 21 4S4 11.611 4 21s7.611 17 17 17Z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          d="M26.657 14.343A7.98 7.98 0 0 0 21 12a7.98 7.98 0 0 0-5.657 2.343m17.879 18.879l8.485 8.485"
-                        />
-                      </g>
-                    </mask>
-                  </defs>
-                  <path
-                    fill="#eca883"
-                    d="M0 0h48v48H0z"
-                    mask="url(#IconifyId192de9132498e907f4)"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className={styles.container_spinner}>
-              <div className={styles.tipo}>
+        </div>
+        <div className={styles.desplegables}>
+          <div className={styles.tipo}>
                 <select id="tipo_de_sexo" name="tipo_de_sexo">
                   <option value="" disabled selected>
                     Sexo
@@ -166,64 +114,6 @@ export default function Perdidos() {
                   <option value="opcion2">Bogotá</option>
                   <option value="opcion3">Medellín</option>
                 </select>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.contenedor_tarjetas}>
-            <div className={styles.container_tarjeta}>
-              <div className={styles.img_fundacion}>
-                <img
-                  src="https://i.pinimg.com/564x/a8/9d/00/a89d00aeb784f5b59b7e8564014c055c.jpg"
-                  alt="img-fundacion"
-                />
-              </div>
-              <div className={styles.info_tarjeta}>
-                <div className={styles.titulos_tarjeta}>
-                  <h3>Nombre fundación</h3>
-                  <h4>Datos de la fundación</h4>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Nombre:</h3>
-                  <p id="Name_animal">nombre</p>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Ciudad:</h3>
-                  <p id="Name_ciudad">ciudad</p>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Contacto:</h3>
-                  <p id="numero_contacto">número</p>
-                </div>
-              </div>
-            </div>
-
-            <div className={styles.container_tarjeta}>
-              <div className={styles.img_fundacion}>
-                <img
-                  src="https://i.pinimg.com/564x/a8/9d/00/a89d00aeb784f5b59b7e8564014c055c.jpg"
-                  alt="img-fundacion"
-                />
-              </div>
-              <div className={styles.info_tarjeta}>
-                <div className={styles.titulos_tarjeta}>
-                  <h3>Nombre fundación</h3>
-                  <h4>Datos de la fundación</h4>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Nombre:</h3>
-                  <p id="Name_animal">nombre</p>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Ciudad:</h3>
-                  <p id="Name_ciudad">ciudad</p>
-                </div>
-                <div className={styles.caja}>
-                  <h3>Contacto:</h3>
-                  <p id="numero_contacto">número</p>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </section>
