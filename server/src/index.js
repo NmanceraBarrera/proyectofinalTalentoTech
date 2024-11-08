69f09b0edc5aecd911aaded21d367c9efec55125
Nicolas <comercialfecor@gmail.com>
2024-11-07 21:05:15 -0500

    ajuste milton

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,11 @@
+const cloudinary = require("cloudinary").v2; // Importa Cloudinary
 const app = require("./app");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Puedes usar variables de entorno para mantenerlo seguro
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(app.get("port"), () => {
   console.log(`Server on port ${app.get("port")}`);
