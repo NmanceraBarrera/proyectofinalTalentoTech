ab497632e3008940e13168f84d308bde816450ac
Milton <miltonacosta1101@gmail.com>
2024-11-08 11:21:53 -0500

    Agregando el formulario de fundaciones, arreglando la seccion de fundaciones y de informacion, ultimos detalles del navbar y añadiendo la ruta del formulario

--- client/src/components/fundaciones/Fundaciones.jsx
+++ client/src/components/fundaciones/Fundaciones.jsx
@@ -20,9 +20,40 @@ export default function Fundaciones() {
               </p>
             </div>
           </section>
-          <section className={styles.containerMain3}>
-            
+          <section className={styles.section_2}>
+            <h3>
+              Aca encontraras todos las mascotas que se han agregado como
+              encontradas, recuerda que: puedes filtrar de acuerdo a ciertas
+              caracteristicas
+            </h3>
+            <div className={styles.barra_busqueda}>
+              <div className={styles.container_busqueda}>
+              <input type="text" name="busqueda" id="busqueda" placeholder="Buscar" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><defs><mask id="IconifyId192f9c235299a8f4b1"><g fill="none" stroke="#fff" stroke-linejoin="round" stroke-width="4"><path fill="#555" d="M21 38c9.389 0 17-7.611 17-17S30.389 4 21 4S4 11.611 4 21s7.611 17 17 17Z"/><path stroke-linecap="round" d="M26.657 14.343A7.98 7.98 0 0 0 21 12a7.98 7.98 0 0 0-5.657 2.343m17.879 18.879l8.485 8.485"/></g></mask></defs><path fill="#eca883" d="M0 0h48v48H0z" mask="url(#IconifyId192f9c235299a8f4b1)"/></svg>
+              </div>
+            </div>
+            <div className={styles.desplegables}>
+              <div className={styles.tipo}>
+                    <select id="ciudad" name="ciudad">
+                      <option value="" disabled selected>
+                        Ciudad
+                      </option>
+                      <option value="opcion2">Bogotá</option>
+                      <option value="opcion3">Medellín</option>
+                    </select>
+              </div>
+            </div>
           </section>
+
+          <section className={styles.section_3}>
+            <div className={styles.tarjetas_animales}>
+              {/* aqui van las tarjetas */}
+            </div>
+            
+            <div className={styles.btn_ver}>
+              <button>Ver Todos</button>
+            </div>
+          </section>  
         </main>
       <Footer/>
     </div>
